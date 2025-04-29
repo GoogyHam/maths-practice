5a7cb5e8db5eba554c1b5f857d1d5fdc37766519
GoogyHam <googy.ham@gmail.com>
2025-04-29 18:28:31 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll(".num-input")
+const inputValues = [];
+
+function takeInputs(){
+
+    inputs.forEach((input, index) => {
+        inputValues[index] = input.value; 
+    });
+    /* Add the inputs from each cell into an array */
+    let inputTotal = "";
+
+    /* Create a blank string variable*/
+    
+    inputValues.forEach((value, subindex) => {
+            inputTotal+= value
+    });
+
+    var inputAsNumber = Number(inputTotal) / 100
+    /* Concatenate each object in the input array onto the string variable */
+
+    document.getElementById("output").innerHTML = "$" + inputAsNumber;
+}
